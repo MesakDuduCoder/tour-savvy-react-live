db41740eba9e4215d6bd8d514bcf7aca3e072eb2
MesakDuduCoder <duduaoa99@gmail.com>
2023-11-09 15:13:42 +0530

    Add

---
+++ src/components/Logout.js
@@ -0,0 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { logoutUser } from '../redux/authentication/userSlice';
+import '../stylesheets/logout.css';
+
+const Logout = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    dispatch(logoutUser()).then(() => {
+      navigate('/');
+    });
+  };
+
+  return (
+    <div className="logout-bg">
+      <button className="logout" type="button" onClick={handleLogout}>Press again to Logout</button>
+    </div>
+  );
+};
+
+export default Logout;
---
+++ src/components/RemoveReservation.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { removeReservation } from '../redux/reservations/reservationSlice';
+import '../stylesheets/delReservation.css';
+import '../stylesheets/common.css';
+
+function RemoveReservation() {
+  const dispatch = useDispatch();
+  const { reservations, isLoading } = useSelector(
+    (store) => store.reservations,
+  );
+  const handleRemove = async (e, id) => {
+    e.preventDefault();
+    dispatch(removeReservation(id));
+  };
+
+  const user = JSON.parse(localStorage.getItem('user'));
+  if (!user) {
+    return <div>Please log in to delete a reservation.</div>;
+  }
+
+  if (isLoading) {
+    return (
+      <>
+        <div>
+          <h2>Loading...</h2>
+        </div>
+      </>
+    );
+  }
+  return (
+    <div className="delReservation">
+      <div className="background" />
+      <div className="main-delReservation">
+        <h2 className="cancel-heading">Cancel a reservation</h2>
+        {reservations.map((reservation) => (
+          <div className="cancel-card" key={reservation.id}>
+            <div className="cancel-details">
+              <h2>
+                Location:&nbsp;
+                {reservation.city_name}
+              </h2>
+              <h3>
+                Date:&nbsp;
+                {reservation.reservation_date}
+              </h3>
+              <h3>
+                Package:&nbsp;
+                {reservation.package_type}
+              </h3>
+            </div>
+            <button
+              type="button"
+              className="form-btns cancel-btn"
+              onClick={(e) => handleRemove(e, reservation.id)}
+            >
+              Cancel
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default RemoveReservation;
